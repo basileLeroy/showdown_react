06d93aafc84c1486f86e4e3e40a8f5297642f689
Basile <basile.leroy.jnm@hotmail.com>
2021-09-08 17:06:19 +0200

    Adding style to ddtail pages

--- src/pages/pokemon.js
+++ src/pages/pokemon.js
@@ -4,12 +4,18 @@ import axios from "axios";
 
 const Pokemon = () => {
     const [details, setDetails] = useState([])
+    const [profileImage, setProfileImage] = useState("")
+    const [name, setName] = useState("")
     const id = new URLSearchParams(useLocation().search).get('id');
+    const pokeName = new URLSearchParams(useLocation().search).get('name');
 
     useEffect(() => {
         axios.get("https://pokeapi.co/api/v2/pokemon/"+id+"/")
             .then(function (res) {
+                console.log(res.data.sprites.other.dream_world.front_default)
                 setDetails(res.data.abilities)
+                setProfileImage(res.data.sprites.other.dream_world.front_default)
+                setName(pokeName)
                 // console.log(res.data.abilities)
             })
             .catch(function (err) {
@@ -22,10 +28,16 @@ const Pokemon = () => {
 
     return (
         <>
-            <div>
-                {details.map((item, index) => {
-                    return <p key={index}>{item.ability.name}</p>
-                })}
+            <div className="flex flex-row w-4/5 mt-32 m-auto h-auto border-8 rounded-lg border-green-300 bg-white bg-opacity-75">
+                <div className="detailLeft flew flex-col">
+                    <h1 className="text-3xl text-center">{name}</h1>
+                    <img src={profileImage} alt=""/>
+                </div>
+                <div className="detailRight">
+                    {details.map((item, index) => {
+                        return <p key={index}>{item.ability.name}</p>
+                    })}
+                </div>
             </div>
         < />
     );
